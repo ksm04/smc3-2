888fe024faf9f317274e65f27cdbf6b56643084c
Administrator <Administrator@501-S01>
2022-04-18 09:36:50 +0900

    0418 프로젝트 추가

---
+++ 0418/src/sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a,b=0;
+		int sum=0;
+		System.out.print("시작 숫자: ");
+		a = scan.nextInt();
+		System.out.print("마지막 숫자: ");
+		b = scan.nextInt();
+		for(int i = a; i <= b; i++) {
+			sum += i;
+		}
+		System.out.println(a+"부터 "+b+"까지의 합은 "+ sum);
+	}
+
+}
